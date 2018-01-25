acda5c131440224d1185eb5a9be1a2a43b2ecc3f
A.Litvinau <A.Litvinau@novacom.by>
2018-01-25 10:05:26 +0300

    master - Changed library for working with postgres

--- science/collector/api/poloniex_public_controller.py
+++ science/collector/api/poloniex_public_controller.py
@@ -1,4 +1,4 @@
-import postgresql
+import psycopg2
 from flask import Flask, g, request
 # from flask_restful import Resource, Api
 import json
@@ -6,11 +6,13 @@ from science.collector.api.utils import json_response
 from science.collector.poloniex import Poloniex
 
 app = Flask(__name__)
+poloniex = Poloniex("APIKey", "Secret".encode())
 
 
 @app.before_request
 def before_request():
-    g.db = postgresql.open(app.config['DATABASE_NAME'])
+    g.db = psycopg2.connect(app.config['DATABASE_NAME'])
+    g.cur = g.db.cursor()
 
 
 @app.route('/public/currencies', methods=['PUT'])
@@ -19,18 +21,18 @@ def update_currencies():
     Synchronizes list of currencies with poloniex api
     :return: elements those were updated
     """
-    poloniex = Poloniex("APIKey", "Secret".encode())
 
     r = poloniex.returnCurrencies()
 
     # clear the table
-    g.db.execute("DELETE FROM poloniex.currencies")
+    g.cur.execute("DELETE FROM poloniex.currencies")
 
-    # insert all the data
-    ins = g.db.prepare(
-        "INSERT INTO poloniex.currencies "
-        "(id, symbol, name, min_conf, deposit_address, disabled, delisted, frozen) "
-        "VALUES ($1, $2, $3, $4, $5, $6, $7, $8)")
+    g.cur.execute("PREPARE currencies_insert_plan AS " +
+                  "INSERT INTO poloniex.currencies "
+                  "(id, symbol, name, min_conf, deposit_address, disabled, delisted, frozen) "
+                  "VALUES ($1, $2, $3, $4, $5, $6, $7, $8)")
+
+    sql = "EXECUTE currencies_insert_plan (%s, %s, %s, %s, %s, %s, %s, %s)"
 
     for symbol, info in r.items():
         identifier: int = info['id']
@@ -41,7 +43,7 @@ def update_currencies():
         delisted: int = info['delisted']
         frozen: int = info['frozen']
 
-        ins(identifier, symbol, name, min_conf, deposit_address, disabled, delisted, frozen)
+        g.cur.execute(sql, (identifier, symbol, name, min_conf, deposit_address, disabled, delisted, frozen))
 
     return json_response(str(r))
 
@@ -70,10 +72,14 @@ def loadMarketTradeHistory():
         start: int = request.args['start']
         end: int = request.args['end']
 
-    poloniex = Poloniex("APIKey", "Secret".encode())
     r = poloniex.returnMarketTradeHistory(currencyPair, start, end)
 
     return json_response(str(r))
+
+
+@app.route('/public/ticker')
+def actualizePairs():
+    pass
 
 
 @app.errorhandler(404)
--- science/collector/run_app.py
+++ science/collector/run_app.py
@@ -2,14 +2,9 @@ import os
 
 from science.collector.api.poloniex_public_controller import app
 
-# from science.collector.api._03_post_method import app
-# from science.collector.api._04_delete_method import app
-# from science.collector.api._05_flask_restful_simple import app
-
-
 if __name__ == '__main__':
     # app.debug = True
-    app.config['DATABASE_NAME'] = 'pq://postgres:postgres@localhost:5432/deep_crypto'
+    app.config['DATABASE_NAME'] = "dbname=deep_crypto user=postgres password=postgres host=localhost port=5432"
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 9090))
     app.run(host=host, port=port)
